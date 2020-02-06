6fbc3dd2a1a75414b442df7dd8358579d871f32b
Eduardo Zortea <eduardozortea9@hotmail.com>
2020-02-06 14:49:55 -0300

    Entidades em EF Feitas :)

---
+++ Entities/ResponseEF.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    /// <summary>
+    /// Representa a resposta de uma operação de banco de dados. (envolve Insert, Update, Delete)
+    /// </summary> 
+    public class Response
+    {
+        public bool Sucesso { get; set; }
+        public List<string> Erros { get; set; }
+
+        public string GetErrorMessage()
+        {
+            StringBuilder builder = new StringBuilder();
+            //Lambda Expression
+            Erros.ForEach(erro => builder.AppendLine(erro));
+            return builder.ToString();
+        }
+
+        public bool HasErrors()
+        {
+            return this.Erros.Count > 0;
+        }
+
+        public Response()
+        {
+            this.Erros = new List<string>();
+        }
+    }
+}
